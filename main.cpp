729aef4db2d9f2a10bac1dc573864f639b9f86eb
Dylan Eck <dylaneck@mymail.mines.edu>
2022-09-20 00:09:27 -0600

    use all three input files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <chrono>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,9 +9,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define EIGEN_MPL2_ONLY
-#include "./eigen-3.4.0/Eigen/Sparse"
-#include "./eigen-3.4.0/Eigen/Dense"
+#define EIGEN_MPL2_ONLY // only use portions of Eigen that have an MPL2 license 
+#include <Eigen/Sparse>
+#include <Eigen/Dense>
 
 struct Element {
     long row;
@@ -60,16 +61,41 @@ void load_matrix_template(const char* filepath, Matrix& matrix) {
     inputFile.close();
 }
 
-Eigen::SparseMatrix<double> ref_format(const Matrix& matrix) {
+void parallel_spmv(Matrix& matrix, Matrix& vector, Matrix& result) {
+    for (int i = 0; i < matrix.numVals; i++) {
+        result[matrix[i].row].value += matrix[i].value * vector[matrix[i].col].value;
+    }
+}
+
+bool verify_result(
+    const Matrix& matrix,
+    Matrix& vector,
+    Matrix& result) {
+
     std::vector< Eigen::Triplet<double> > triplets;
-    triplets.reserve(matrix.numRows);
+    triplets.reserve(matrix.numVals);
     for (const Element& element : matrix.elements) {
-        triplets.push_back(Eigen::Triplet<double>(element.row, element.col, element.value));
+        triplets.push_back(
+            Eigen::Triplet<double>(element.row, element.col, element.value)
+        );
     }
 
     Eigen::SparseMatrix<double> ref_mat(matrix.numRows, matrix.numCols);
     ref_mat.setFromTriplets(triplets.begin(), triplets.end());
-    return ref_mat;
+
+    Eigen::VectorXd ref_vec(matrix.numCols);
+    for (int i = 0; i < vector.numRows; i++) {
+        ref_vec(i) = vector[i].value;
+    }
+
+    Eigen::VectorXd ref_res = ref_mat * ref_vec;
+
+    for (int i = 0; i < vector.numVals; i++) {
+        if (std::abs(result[i].value - ref_res(i)) > 0.1f) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main(int argc, char const *argv[])
@@ -77,56 +103,74 @@ int main(int argc, char const *argv[])
     std::srand(std::time(NULL));
     std::rand();
 
-    Matrix matrix;
-    load_matrix_template("./NLR.mtx", matrix);
-    Eigen::SparseMatrix<double> ref_matrix = ref_format(matrix);
+    const char *input_files[3] = {
+        "NLR.mtx",
+        "channel-500x100x100-b050.mtx",
+        "delaunay_n19.mtx"
+    };
 
-    Matrix vec;
-    vec.numRows = matrix.numCols;
-    vec.numCols = 1;
-    vec.elements = std::vector<Element>(vec.numRows);
+    float exec_times[3]{};
 
-    Eigen::VectorXd ref_vec(matrix.numCols);
+    for (int i = 0; i < 3; i++) {
+        Matrix matrix;
+        load_matrix_template(input_files[i], matrix);
 
-    for (int i = 0; i < vec.numRows; i++) {
-        vec[i] = Element{i, 0, (double)rand() / RAND_MAX * 4.8f + 0.1f};
-        ref_vec(i) = vec[i].value;
-    }
+        Matrix vec;
+        vec.numRows = matrix.numCols;
+        vec.numCols = 1;
+        vec.numVals = matrix.numCols;
+        vec.elements = std::vector<Element>(vec.numVals);
 
-    Matrix res;
-    res.numRows = matrix.numCols;
-    res.numCols = 1;
-    res.elements = std::vector<Element>(res.numRows);
+        for (int i = 0; i < vec.numRows; i++) {
+            vec[i] = Element{i, 0, (double)rand() / RAND_MAX * 4.8f + 0.1f};
+        }
 
-    Eigen::VectorXd ref_res(matrix.numCols);
-    
-    ref_res = ref_matrix * ref_vec;
+        Matrix res;
+        res.numRows = matrix.numCols;
+        res.numCols = 1;
+        res.numRows = matrix.numCols;
+        res.elements = std::vector<Element>(res.numRows);
 
-    auto startTimePoint = std::chrono::high_resolution_clock::now();
-    
-    for (int i = 0; i < matrix.elements.size(); i++) {
-        res[matrix[i].row].value += matrix[i].value * vec[matrix[i].col].value;
-    }
+        auto startTimePoint = std::chrono::high_resolution_clock::now();
 
-    auto endTimePoint = std::chrono::high_resolution_clock::now();
+        parallel_spmv(matrix, vec, res);
 
-    auto tstart = std::chrono::time_point_cast<std::chrono::microseconds>(startTimePoint).time_since_epoch().count();
-    auto tend = std::chrono::time_point_cast<std::chrono::microseconds>(endTimePoint).time_since_epoch().count();
+        auto endTimePoint = std::chrono::high_resolution_clock::now();
 
-    float duration = (tend - tstart) * 0.001f;
-    std::cout << "multiplication execution time: " << duration << "ms" << std::endl;
+        auto tstart =
+            std::chrono::time_point_cast<std::chrono::microseconds>(startTimePoint)
+            .time_since_epoch()
+            .count();
 
-    bool answer_correct = true;
-    for (int i = 0; i < vec.numRows; i++) {
-        if (std::abs(res[i].value - ref_res(i)) > 0.1f) {
-            std::cout << "result is not correct (row " << i << "): " << res[i].value << " != " << ref_res(i) << std::endl;
-            answer_correct = false;
-            break;
+        auto tend =
+            std::chrono::time_point_cast<std::chrono::microseconds>(endTimePoint)
+            .time_since_epoch()
+            .count();
+
+        float duration = (tend - tstart) * 0.001f;
+        exec_times[i] = duration;
+
+        bool correct = verify_result(matrix, vec, res);
+        if (!correct) {
+            throw std::runtime_error(
+                "matrix multiplication produced incorrect result"
+            );
         }
     }
 
-    if (answer_correct) {
-        std::cout << "result is correct" << std::endl;
+    for (int i = 0; i < 3; i++) {
+        std::cout
+            << std::setw(30)
+            << std::left
+            << input_files[i];
+
+        std::cout
+            << std::setw(6)
+            << std::right
+            << std::fixed
+            << std::setprecision(0)
+            << exec_times[i] << "ms"
+            << std::endl;
     }
 
     return EXIT_SUCCESS;
